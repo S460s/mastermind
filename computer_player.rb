eb7a52b9cf77e163a1eda75969fd34bc6e702818
S460 <rommel.e@protonmail.ch>
2022-05-25 18:30:03 +0300

    computer player class

---
+++ computer_player.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative 'player'
+
+# A computer player which will generate outputs.
+class ComputerPlayer < Player
+  def generate_code(options)
+    options.shuffle.take(4)
+  end
+end
