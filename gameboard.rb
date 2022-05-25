fbdca6d6610d4a40d28fb2da2529888e35a230da
S460 <rommel.e@protonmail.ch>
2022-05-25 19:17:36 +0300

    add gameboard functionality

--- gameboard.rb
+++ gameboard.rb
@@ -5,14 +5,47 @@ class Gameboard
   OPTIONS = %w[red blue yellow green purple orange].freeze
   TURNS = 12
 
-  def initialize
+  def initialize(code_maker, code_breaker)
     @turns_played = 0
+    @code_maker = code_maker
+    @code_breaker = code_breaker
+
+    @used_indexes = []
     @secret_code = []
     @feedback = []
     @board = []
   end
 
-  def init_code(player)
-    @secret_code = player.generate_code(OPTIONS)
+  def init_code
+    @secret_code = @code_maker.generate_code(OPTIONS)
+    p @secret_code
+  end
+
+  def wins?
+    @feedback.all?('black') && @feedback.length == @secret_code.length
+  end
+
+  def gameover?
+    wins? || @turns_played == TURNS
+  end
+
+  def start_game
+    init_code
+    loop do
+      make_guess
+      if gameover?
+        puts 'GG'
+        break
+      end
+    end
+  end
+
+  def make_guess
+    @feedback = @code_breaker.guess(OPTIONS).map.with_index do |value, index|
+      if @secret_code.include?(value)
+        @secret_code[index] == value ? 'black' : 'white'
+      end
+    end
+    @feedback.shuffle!.compact!
   end
 end
